a772a533ac743bb020265757fa5b691e5648ddd3
ukaveri <168158849+ukaveri@users.noreply.github.com>
2024-06-04 18:49:41 +0530

    Add files via upload

---
+++ practive03.06.2024.py
@@ -0,0 +1,31 @@
+'''
+if 5 > 2:
+    print("five is greater than two")
+        print("Five is greater than two!")
+'''
+#if we give unwanted spaces we get error of same block.
+'''
+x = int(five)
+print(x)
+'''
+#here five is treated as a variable
+'''
+t = (2,)
+print(t)
+'''
+#here if only one value is taken then it takes as single variable
+#may be an integer or string but when taken as t = (2, )now treated
+#as tuple and tuple is inmutable.
+'''
+a = "tuple"
+A = "Tuple"
+print(a)
+print(A)
+'''
+#python variables are case-sensitive.
+
+
+
+
+
+
